9f1052e846676f4723bcca3c7c07ee0ec86db97f
heyrict <xiezh0831@126.com>
2017-09-27 12:56:44 +0800

    add simple profile/mondai_show pages with a simple css

--- sui_hei/urls.py
+++ sui_hei/urls.py
@@ -6,5 +6,7 @@ app_name = "sui_hei"
 
 urlpatterns = [
     url(r"^$", views.index, name="index"),
-    url(r"^mondai$", views.mondai, name="index"),
+    url(r"^mondai$", views.mondai, name="mondai"),
+    url(r"^mondai/show/(?P<mondai_inst>[0-9]+)$", views.mondai_show, name="mondai_show"),
+    url(r"^profile/(?P<user_inst>.+)$", views.profile, name="profile"),
 ]
--- sui_hei/views.py
+++ sui_hei/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Mondais, Shitumons, Users, Chats
@@ -12,3 +12,13 @@ def mondai(request):
     template = loader.get_template('sui_hei/mondai.html')
     mondai_list = Mondais.objects.order_by('-created')
     return HttpResponse(template.render({'mondai_list': mondai_list}, request))
+
+def mondai_show(request, mondai_inst):
+    template = loader.get_template('sui_hei/mondai_show.html')
+    mondai_inst = get_object_or_404(Mondais, pk=mondai_inst)
+    return HttpResponse(template.render({'mondai': mondai_inst}, request))
+
+def profile(request, user_inst):
+    template = loader.get_template('sui_hei/profile.html')
+    user_inst = get_object_or_404(Users, name=user_inst)
+    return HttpResponse(template.render({'user': user_inst}, request))
