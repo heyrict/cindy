fca922cc783474056bf74b536c79f2b3c76d0593
heyrict <xiezh0831@126.com>
2017-09-28 17:25:51 +0800

    Simplification works by using templates

--- sui_hei/urls.py
+++ sui_hei/urls.py
@@ -6,9 +6,9 @@ app_name = "sui_hei"
 
 urlpatterns = [
     url(r"^$", views.index, name="index"),
-    url(r"^mondai$", views.mondai, name="mondai"),
-    url(r"^mondai/show/(?P<mondai_inst>[0-9]+)$", views.mondai_show, name="mondai_show"),
-    url(r"^profile/(?P<user_inst>[0-9]+)$", views.profile, name="profile"),
+    url(r"^mondai$", views.MondaiView.as_view(), name="mondai"),
+    url(r"^mondai/show/(?P<pk>[0-9]+)$", views.MondaiShowView.as_view(), name="mondai_show"),
+    url(r"^profile/(?P<pk>[0-9]+)$", views.ProfileView.as_view(), name="profile"),
     url(r"^users/add$", views.users_add, name="users_add"),
     url(r"^users/login$", views.users_login, name="users_login"),
     url(r"^users/logout$", views.users_logout, name="users_logout"),
