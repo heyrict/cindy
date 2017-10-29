8141b5975b303c26298c2d83b37e5ff9a496393f
heyrict <xiezh0831@126.com>
2017-10-29 12:03:40 +0800

    Rewrite sidebar.js

--- sui_hei/static/sidebar.js
+++ sui_hei/static/sidebar.js
@@ -17,21 +17,73 @@ require(["jquery", "velocity"], function($) {
   $(document).ready(function() {
     ToggleSidebar();
   });
-  function ToggleSidebar() {
-    // Set mode on load
-    var $leftbar = $(".leftbar");
 
-    // Calculate best width & height of sidebar
-    goodh = window.innerHeight - 20;
-    if (goodh < 400) {
-      goodh = window.innerHeight;
+  $(window).on("load", function() {
+    //Calculate the good width&height, resize, with toggle event handles
+    CalcGoodRect();
+    ResizeSidebar();
+    ResizeSidebarContent();
+  });
+
+  $(window).on("resize", function() {
+    // Recalculate the good width/height and resize
+    CalcGoodRect();
+    ResizeSidebar();
+    ResizeSidebarContent();
+  });
+});
+
+function ResizeSidebarContent() {
+  require(["jquery", "velocity"], function($) {
+    // Set Height
+    var nav_height = 60;
+    var chat_height = 60;
+    var table_height = goodh - nav_height - chat_height - 5;
+    $(".lobby_navigation_div").css("height", nav_height + "px");
+    $(".lobby_chat_div").css("height", nav_height + "px");
+    $(".lobby_table_div").css("height", table_height + "px");
+
+    $("#lobby_chat_input").on("keypress", function(e) {
+      if (e.which == 13) {
+        $("#lobby_chat_submit").click();
+      }
+    });
+
+    lobby_chat_can_submit = true;
+    $("#lobby_chat_submit").on("click", function() {
+      if (!lobby_chat_can_submit) {
+        return false;
+      }
+      lobby_chat_can_submit = false;
+      PostChat("{{ channel }}", $("#lobby_chat_input").val());
+    });
+
+    $(".lobby_table_div").velocity("scroll", {
+      offset: $(".lobby_table_div").prop("scrollHeight"),
+      container: $(".lobby_table_div"),
+      duration: 0
+    });
+  });
+}
+
+function ResizeSidebar() {
+  require(["jquery", "velocity"], function($) {
+    var $leftbar = $(".leftbar");
+    $leftbar.css("width", goodw + "px");
+    if ($leftbar.attr("mode") == "open") {
+      $leftbar.css("left", "0%");
+    } else {
+      $leftbar.css("left", -goodw * 0.792 + "px");
+      $leftbar.attr("mode", "closed");
     }
-    windoww = Math.max(window.outerWidth, window.innerWidth);
-    goodw = windoww * 0.45;
-    small_screen = false;
-    if (goodw < 400) {
-      goodw = Math.max(window.outerWidth, window.innerWidth);
-      small_screen = true;
+
+    var $rightbar = $(".rightbar");
+    $rightbar.css("width", goodw + "px");
+    if ($rightbar.attr("mode") == "open") {
+      $rightbar.css("left", windoww - goodw * 0.792 + "px");
+    } else {
+      $rightbar.css("left", windoww + "px");
+      $rightbar.attr("mode", "closed");
     }
 
     $(".leftbar_content").css("height", goodh + "px");
@@ -47,83 +99,100 @@ require(["jquery", "velocity"], function($) {
     $memobarbtn
       .css("height", $memobarbtn.height() / resize_rate + "px")
       .css("width", $memobarbtn.width() / resize_rate + "px");
+  });
+}
 
+var goodh, goodw, windoww, small_screen;
+function CalcGoodRect() {
+  // Calculate best width & height of sidebar
+  goodh = window.innerHeight - 20;
+  if (goodh < 400) {
+    goodh = window.innerHeight;
+  }
+  windoww = Math.max(window.outerWidth, window.innerWidth);
+  goodw = windoww * 0.45;
+  small_screen = false;
+  if (goodw < 400) {
+    goodw = Math.max(window.outerWidth, window.innerWidth);
+    small_screen = true;
+  }
+}
+
+function ToggleSidebar() {
+  require(["jquery", "velocity"], function($) {
     // Toggle sidebar
     $(".leftbar_button").on("click", function() {
-      var $this = $(".leftbar");
+      var $leftbar = $(".leftbar");
+      var $rightbar = $(".rightbar");
 
-      if ($this.attr("mode") == "closed") {
-        $this.velocity({
+      if ($leftbar.attr("mode") == "closed") {
+        $leftbar.velocity({
           left: "0%",
-          width: goodw + "px"
+          duration: 1000
         });
-        $this.attr("mode", "open");
+        $leftbar.attr("mode", "open");
 
         if (small_screen) {
-          $(".rightbar").velocity({
-            left: windoww + goodw + "px"
+          $rightbar.velocity({
+            left: windoww + goodw + "px",
+            duration: 1000
           });
-          $(".rightbar").attr("mode", "closed");
+          $rightbar.attr("mode", "hide");
         }
-      } else if ($this.attr("mode") == "open") {
+      } else if ($leftbar.attr("mode") == "open") {
         $(".leftbar").velocity({
           left: -goodw * 0.792 + "px",
-          width: goodw + "px"
+          duration: 1000
         });
-        $this.attr("mode", "closed");
+        $leftbar.attr("mode", "closed");
 
         if (small_screen) {
-          $(".rightbar").velocity({
-            left: windoww + "px"
+          $rightbar.velocity({
+            left: windoww + "px",
+            duration: 1000
           });
-          $(".rightbar").attr("mode", "closed");
+          $rightbar.attr("mode", "closed");
         }
       }
     });
-
     $(".rightbar_button").on("click", function() {
       var $this = $(".rightbar");
 
       if ($this.attr("mode") == "closed") {
         $this.velocity({
           left: windoww - goodw * 0.792 + "px",
-          width: goodw + "px"
+          duration: 1000
         });
         $this.attr("mode", "open");
 
         if (small_screen) {
           $(".leftbar").velocity({
-            left: -goodw + "px"
+            left: -goodw + "px",
+            duration: 1000
           });
-          $(".leftbar").attr("mode", "closed");
+          $(".leftbar").attr("mode", "hide");
         }
       } else if ($this.attr("mode") == "open") {
         $(".rightbar").velocity({
           left: windoww + "px",
-          width: goodw + "px"
+          duration: 1000
         });
         $this.attr("mode", "closed");
 
         if (small_screen) {
           $(".leftbar").velocity({
             left: -goodw * 0.792 + "px",
-            width: goodw + "px"
+            duration: 1000
           });
           $(".leftbar").attr("mode", "closed");
         }
       }
     });
-  }
-});
+  });
+}
 
 function ChangeChannel() {
-  jQuery.get(
-    "/lobby/channel",
-    { channel: jQuery("#lobby_nav_input").val() },
-    function(data) {
-      jQuery(".leftbar_content").html(data);
-    }
-  );
+  OpenChat(jQuery("#lobby_nav_input").val());
   return false;
 }
 
@@ -187,14 +256,14 @@ function OpenChat(channel) {
   if ($this.attr("mode") == "closed") {
     $this.velocity({
       left: "0%",
-      width: goodw + "px"
+      duration: 1000
     });
     $this.attr("mode", "open");
     if (small_screen) {
       $(".rightbar").velocity({
         left: windoww + goodw + "px"
       });
-      $(".rightbar").attr("mode", "closed");
+      $(".rightbar").attr("mode", "hide");
     }
   }
 }
