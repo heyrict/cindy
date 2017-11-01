fdf28b0b0a8599531d95c5fc09b341d31447d529
heyrict <xiezh0831@126.com>
2017-11-01 12:49:10 +0800

    Fix next-links & editing-after-answered in mondai_show

--- sui_hei/static/js/app/mondai_show.js
+++ sui_hei/static/js/app/mondai_show.js
@@ -1,13 +1,15 @@
-require(['marked', 'jquery'], function(marked, $) {
+require(["marked", "jquery"], function(marked, $) {
   $(document).ready(function() {
-    $("#kaisetu_preview")[0].innerHTML = marked($("#kaisetu_textarea").val());
+    if ($("#kaisetu_textarea").length > 0) {
+      $("#kaisetu_preview").html(marked($("#kaisetu_textarea").val()));
+    }
 
     $("#kaisetu_textarea").on("input", function() {
-      $("#kaisetu_preview")[0].innerHTML = marked($("#kaisetu_textarea").val());
+      $("#kaisetu_preview").html(marked($("#kaisetu_textarea").val()));
     });
 
     $("#memo_textarea").on("input", function() {
-      $("#memo_preview")[0].innerHTML = marked($("#memo_textarea").val());
+      $("#memo_preview").html(marked($("#memo_textarea").val()));
     });
     var PageURL = window.location.pathname;
     $("a.qna_edit").each(function() {
