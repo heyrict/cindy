576a405a0cb762befd5fd9803ed521dc3b1af06c
heyrict <xiezh0831@126.com>
2017-11-06 16:33:36 +0800

    Separate js from html script tag

---
+++ sui_hei/static/js/app/profile_mystar.js
@@ -0,0 +1,30 @@
+require(["jquery", "bootstrap", "bootbox"], function($) {
+  function init() {
+    $(".remove_star_button").on("click", function() {
+      var csrftoken = $("[name=csrfmiddlewaretoken]").val();
+      var star_id = this.value;
+      bootbox.confirm(
+        `<p>${gettext(
+          "You cannot revert change if you delete the star!"
+        )}</p><p>${gettext("Continue?")}</p>`,
+        function(conf) {
+          if (conf) {
+            jQuery.post(
+              "{% url 'sui_hei:mondai_star_remove' %}",
+              {
+                csrfmiddlewaretoken: csrftoken,
+                star_id: star_id
+              },
+              function(data) {
+                if (data) {
+                  location.reload();
+                }
+              }
+            );
+          }
+        }
+      );
+    });
+  }
+  init();
+});
